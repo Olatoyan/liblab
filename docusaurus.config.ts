369a17f046360c9c77db30e1efd11afd23eef504
Olatoyan <golatoyan@gmail.com>
2024-06-17 21:08:30 +0100

    fixing production issues

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
   favicon: "img/liblab.svg",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://olatoyan.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -15,10 +15,12 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "olatoyan", // Usually your GitHub org/user name.
-  projectName: "liblab", // Usually your repo name.
+  projectName: "olatoyan.github.io", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
+  deploymentBranch: "main",
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
